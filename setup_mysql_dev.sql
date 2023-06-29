8a5bf103007beb5e6e5f826ed60e1a1da22a0b23
root <tayler.coon@gmail.com>
2023-06-29 12:33:35 -0700

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,1 +1,6 @@
--- setup that prepares a MySQL server for the project
+-- setup that prepares a MySQL server for the project-- setup that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* AND hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
