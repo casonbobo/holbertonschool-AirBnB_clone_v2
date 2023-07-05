24d48724a82f9a440292ccd24b34d853a50b6fab
tayler.coon@gmail.com <tayler.coon@gmail.com>
2023-07-05 09:25:38 -0700

    fixing task 7

--- models/user.py
+++ models/user.py
@@ -15,8 +15,6 @@ class User(BaseModel):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                                cascade="all, delete, delete-orphan")
 
     else:
         email = ""
