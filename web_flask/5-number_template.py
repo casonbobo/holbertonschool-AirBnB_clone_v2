7190e570eb093706b440353770063b3fa741ba38
casonbobo <casonsbobo@gmail.com>
2023-07-17 13:08:12 -0700

    i have to hope this is right

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,7 +2,7 @@
 """This is my documentation"""
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -29,6 +29,10 @@ def python(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -28,28 +28,19 @@ def python(text):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    if isinstance(n, int):
         return f"{n} is a number"
-    else:
-        return f"{n} is not a valid integer"
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    if isinstance(n, int):
         return render_template('number.html', number=n)
-    else:
-        return f"{n} is not a valid integer"
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    if isinstance(n, int):
-        if n % 2 == 0:
-            number_type = 'even'
-        else:
-            number_type = 'odd'
-        return render_template('number_odd_or_even.html', number=n, type=number_type)
+def odd_or_even_template(n):
+    if n % 2 == 0:
+        text = 'even'
     else:
-        return f"{n} is not a valid integer"
+        text = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, text=text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
