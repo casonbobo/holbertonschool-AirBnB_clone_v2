ced81d7be0b445a4e9adba10fe4cb424dd5d0cba
casonbobo <casonsbobo@gmail.com>
2023-07-17 13:20:18 -0700

    changed the line counts

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,14 +11,17 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     formatted_text = text.replace('_', ' ')
     return f"C {formatted_text}"
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -26,13 +29,16 @@ def python(text):
     formatted_text = text.replace('_', ' ')
     return f"Python {formatted_text}"
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -11,14 +11,17 @@ app = Flask(__name__)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     formatted_text = text.replace('_', ' ')
     return f"C {formatted_text}"
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -26,13 +29,16 @@ def python(text):
     formatted_text = text.replace('_', ' ')
     return f"Python {formatted_text}"
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
         return f"{n} is a number"
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
         return render_template('number.html', number=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even_template(n):
@@ -41,6 +47,7 @@ def odd_or_even_template(n):
     else:
         text = 'odd'
     return render_template('6-number_odd_or_even.html', n=n, text=text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
