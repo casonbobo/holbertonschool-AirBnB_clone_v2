fcb1b02364d926cdcef7f8998673d1fcb028fb10
casonbobo <casonsbobo@gmail.com>
2023-07-18 12:28:19 -0700

    this is a test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web app"""
+"""This is my documentation maybe you need a longer comment"""
 from flask import Flask, render_template
 from models import storage
 
@@ -7,12 +7,14 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_db(exception):
+    """This for tearing down, not up"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """This is for the states"""
     states = storage.all("State")
     return render_template('7-states_list.html', states=states.values())
 
