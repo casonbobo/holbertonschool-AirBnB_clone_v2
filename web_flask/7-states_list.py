77682f61e33533d14fd5e0577d093cc94bc2d739
casonbobo <casonsbobo@gmail.com>
2023-07-18 11:13:37 -0700

    this is a little galic knots

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""This is my documentation maybe you need a longer comment"""
-
-
+"""This is my documentation maybe you need a longer comment """
 from flask import Flask, render_template
 from models import storage, State
 
 app = Flask(__name__)
-
+    
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """This for tearing down, not up"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """This is for the states"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda state: state.name)
-
     return render_template('states_list.html', states=sorted_states)
 
 
