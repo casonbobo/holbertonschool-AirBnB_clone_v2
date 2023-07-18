9c40daa1dd39f9047adb2f7335bc8c9319d8fddd
casonbobo <casonsbobo@gmail.com>
2023-07-18 10:04:04 -0700

    this should be ok

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,58 +1,22 @@
 #!/usr/bin/python3
 """This is my documentation"""
 from flask import Flask, render_template
+from models import storage, State
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
 
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    formatted_text = text.replace('_', ' ')
-    return f"C {formatted_text}"
-
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    formatted_text = text.replace('_', ' ')
-    return f"Python {formatted_text}"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    if isinstance(n, int):
-        return f"{n} is a number"
-    else:
-        return f"{n} is not a valid integer"
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    if isinstance(n, int):
-        return render_template('number.html', number=n)
-    else:
-        return f"{n} is not a valid integer"
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    if isinstance(n, int):
-        if n % 2 == 0:
-            number_type = 'even'
-        else:
-            number_type = 'odd'
-        return render_template('number_odd_or_even.html', number=n, type=number_type)
-    else:
-        return f"{n} is not a valid integer"
+    return render_template('states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
